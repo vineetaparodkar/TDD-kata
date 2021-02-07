ef8e6b811044fe309b034f927ba5d7e39a3facdd
vineeta parodkar <vineetaparodkar@gmail.com>
2021-02-07 16:13:22 +0530

    updated printNumber function to fix failing buzz test case

--- src/main/java/com/sample/kata/FizzBuzz.java
+++ src/main/java/com/sample/kata/FizzBuzz.java
@@ -5,7 +5,7 @@ public class FizzBuzz {
     public String printNumber() {
         String result = " ";
         for (int i = 1; i <= 100; i++) {
-            result += i%3 == 0 ? " " + "Fizz" : " " + i;
+            result += i % 3 == 0 ? " " + "Fizz" : i % 5 == 0 ? " " + "Buzz" : " " + i;
 
         }
         return result;
