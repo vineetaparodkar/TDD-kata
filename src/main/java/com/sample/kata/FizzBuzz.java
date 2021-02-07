f356f77834614e731d3a42d43d044ccd77d3a839
vineeta parodkar <vineetaparodkar@gmail.com>
2021-02-07 15:40:06 +0530

    Added function printNumbers and failing test case for printing fizz

--- src/main/java/com/sample/kata/FizzBuzz.java
+++ src/main/java/com/sample/kata/FizzBuzz.java
@@ -3,6 +3,11 @@ package com.sample.kata;
 public class FizzBuzz {
 
     public String printNumber() {
-        return null;
+        String result = " ";
+        for (int i = 1; i <= 100; i++) {
+            result = result + " " + i;
+
+        }
+        return result;
     }
 }
--- src/test/java/com/sample/kata/FizzBuzzTest.java
+++ src/test/java/com/sample/kata/FizzBuzzTest.java
@@ -1,6 +1,7 @@
 package com.sample.kata;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,4 +20,12 @@ public class FizzBuzzTest {
     @Test
     public void printNumber() {
     }
+
+    //Test to return fizz if number divisible by 3
+    @Test
+    public void printFizz() {
+        String result = new FizzBuzz().printNumber();
+        Assert.assertTrue(result.contains("Fizz"));
+    }
+
 }
